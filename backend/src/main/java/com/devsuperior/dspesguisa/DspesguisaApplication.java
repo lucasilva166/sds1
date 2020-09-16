6a65a948299715631ce89ff7363f34bf88fe9ec8
lucasilva166 <jakeblack208@gmail.com>
2020-09-16 13:30:05 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dspesguisa/DspesguisaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dspesguisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DspesguisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DspesguisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dspesguisa/DspesguisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dspesguisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesguisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
